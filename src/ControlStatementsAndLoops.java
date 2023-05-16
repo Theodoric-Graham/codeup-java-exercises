8be27ca8418248f50458c7b1f148fcb9e13fc817
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-16 09:47:54 -0500

    initial control statements exercise

---
+++ src/ControlStatementsAndLoops.java
@@ -0,0 +1,9 @@
+public class ControlStatementsAndLoops {
+    public static void main(String[] args) {
+        System.out.println(10 > 5); //true
+        System.out.println(5 == 5); //true
+        System.out.println(true == false); //false
+        System.out.println(2 <= 2); //true
+        //Strings are not mutable
+    }
+}
