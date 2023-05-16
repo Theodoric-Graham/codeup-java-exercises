f4e57d330b3fb9d5b56e81aeeb6562460e23c70e
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-16 09:47:35 -0500

    notes for lecture

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        //1a
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+        //1b
+        int
+
+    }
+}
