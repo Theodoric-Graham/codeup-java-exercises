188a54b44b17676f958e62538d1abbc3185a1e86
Theodoric Graham <theodoricgraham@gmail.com>
2023-05-30 15:58:52 -0500

    finished collections exercise with bonus

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -4,9 +4,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class GradesApplication {
-    public static boolean confirm;
     public static Scanner sc = new Scanner(System.in);
-    public static int classAverageSum;
     public static HashMap<String, Student> students = new HashMap<>();
 
 
@@ -19,63 +17,6 @@ public class GradesApplication {
         createGithubList();
         selectStudent();
         System.out.println("Goodbye and have a good day!");
-    }
-
-    private static void selectStudent() {
-        do {
-            System.out.println("What student would you like to see more information on?");
-            String choice = sc.nextLine();
-            if(students.containsKey(choice)) {
-                System.out.printf("Name: %s - GitHub Username: %s \n", students.get(choice).getName(), choice );
-                System.out.println("Student average: " + students.get(choice).getGradeAverage());
-//                System.out.println(students.get(choice));
-                System.out.println("All grades: " + students.get(choice).getGrades());
-                System.out.println("Would you like to see another student? [y/n]");
-                boolean input = sc.nextLine().equalsIgnoreCase("y");
-                confirm = input;
-
-            } else {
-                System.out.printf("Sorry, no student found with the name of %s \n", choice);
-                System.out.println("Would you like to see another student? [y/n]");
-                boolean input = sc.nextLine().equalsIgnoreCase("y");
-                confirm = input;
-
-            }
-        } while (confirm);
-    }
-
-    private static void createGithubList() {
-        System.out.println("Here are the GitHub usernames of our students:");
-        String githubUsers = "";
-        for (String student : students.keySet()) {
-            githubUsers += "|" + student + "| ";
-        }
-
-        System.out.println(githubUsers);
-    }
-
-    private static void csvPrompt() {
-        System.out.println("Would you like to view a csv report of all the students? [y/n]");
-        boolean csvReport = sc.nextLine().equalsIgnoreCase("y");
-
-        if(csvReport) {
-            System.out.println("name, github_username, average");
-            for (String student : students.keySet()) {
-                System.out.printf("%s, %s, %.2f \n",                 students.get(student).getName(), student, students.get(student).getGradeAverage());
-            }
-        }
-    }
-
-    private static void classAveragePrompt() {
-        System.out.println("Would you like to view the class average? [y/n]");
-        boolean classAverage = sc.nextLine().equalsIgnoreCase("y");
-
-        if(classAverage) {
-            for (Student student: students.values()) {
-                classAverageSum += student.getGradeAverage();
-            }
-            System.out.println(classAverageSum / students.size());
-        }
     }
 
     private static void createStudents() {
@@ -110,6 +51,76 @@ public class GradesApplication {
             }
         }
     }
+
+    private static void classAveragePrompt() {
+
+        int classAverageSum = 0;
+
+        System.out.println("Would you like to view the class average? [y/n]");
+        boolean classAverage = sc.nextLine().equalsIgnoreCase("y");
+
+        if(classAverage) {
+            for (Student student: students.values()) {
+                classAverageSum += student.getGradeAverage();
+            }
+            System.out.println(classAverageSum / students.size());
+        }
+    }
+
+    private static void csvPrompt() {
+        System.out.println("Would you like to view a csv report of all the students? [y/n]");
+        boolean csvReport = sc.nextLine().equalsIgnoreCase("y");
+        if(csvReport) {
+            System.out.println("name, github_username, average");
+            for (String student : students.keySet()) {
+                System.out.printf("%s, %s, %.2f \n", students.get(student).getName(), student, students.get(student).getGradeAverage());
+            }
+        }
+    }
+
+    private static void createGithubList() {
+        System.out.println("Here are the GitHub usernames of our students:");
+        String githubUsers = "";
+        for (String student : students.keySet()) {
+            githubUsers += "|" + student + "| ";
+        }
+
+        System.out.println(githubUsers);
+    }
+
+    private static void selectStudent() {
+        boolean confirm;
+        do {
+            System.out.println("What student would you like to see more information on?");
+            String choice = sc.nextLine();
+            if(students.containsKey(choice)) {
+                System.out.printf("Name: %s - GitHub Username: %s \n", students.get(choice).getName(), choice );
+                System.out.println("Student average: " + students.get(choice).getGradeAverage());
+//                System.out.println(students.get(choice));
+                System.out.println("All grades: " + students.get(choice).getGrades());
+                System.out.println("Would you like to see another student? [y/n]");
+                boolean input = sc.nextLine().equalsIgnoreCase("y");
+                confirm = input;
+
+            } else {
+                System.out.printf("Sorry, no student found with the name of %s \n", choice);
+                System.out.println("Would you like to see another student? [y/n]");
+                boolean input = sc.nextLine().equalsIgnoreCase("y");
+                confirm = input;
+
+            }
+        } while (confirm);
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 }
